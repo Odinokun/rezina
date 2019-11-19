a9daa784c7f32c836ee6a325c7a1cfec006b5f0d
odinokun <odinokun@gmail.com>
2019-11-19 19:48:37 +0200

    start

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+$(function() {
+
+  // begin SVG for IE
+  require('./modules/svg4everybody')();
+
+  // begin Slick slider
+  require('./modules/slick')();
+
+  // begin Popup
+  require('./modules/popup')();
+
+});
